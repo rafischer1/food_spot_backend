0e139164cb487fa7f218abc907d07d36ebdbfd26
Robert Fischer <artiefischer@gmail.com>
2018-10-27 12:15:30 -0600

    add a single whitepsace charcter and then deleted it causing the need for an update

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const knex = require('../../knex')
-const ctrl = require('../controllers/posts')
 
 router.get('/', ctrl.getAll)
 router.post('/', ctrl.create)
 router.patch('/', ctrl.patch)
 router.delete('/', ctrl.delete)
 
-module.exports = router;.exports = router;
+module.exports = router;

3edef3ef6cfe78fa3a07b702f14a1798ec48074e
Andy Chuong <andy.chuong@colorado.edu>
2018-10-27 19:20:00 -0600

    posts seed

--- seeds/02_posts.js
+++ seeds/02_posts.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-exports.seed = function(knex, Promise) {
-
-  return knex('posts').del()
-    .then(() => {
-
-      return knex('posts').insert([
-          { id: 1, foodName: "test", address: "100 Main Street", city: "Boulder", state: 'CO', zipcode: 80304, country: 'USA', imageUrl: 'http://placehold.jp/150x150.png', promoted: false }
-        ])
-        .then(() => {
-
-          return knex.raw("SELECT setval('posts_id_seq', (SELECT MAX(id) FROM posts))")
-        })
-    })
-}
-
-// new Date(year, month, date, hours, minutes, seconds, ms)
-=======
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('posts').del()
@@ -63,4 +44,3 @@ exports.seed = function(knex, Promise) {
        `SELECT setval('posts_id_seq', (SELECT MAX(id) FROM posts));`)
    })
 };
->>>>>>> posts-seed

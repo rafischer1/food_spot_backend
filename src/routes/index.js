b8fbd788bfe4697b00c3df7f561390156c75a3e6
Robert Fischer <artiefischer@gmail.com>
2018-10-29 12:02:14 -0600

    ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const knex = require('../../knex')
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { session: req.session, user: req.user });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;

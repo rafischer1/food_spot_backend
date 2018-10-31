37fa82b1537fa9e3d1b86be16c69ae78aef98045
Robert Fischer <artiefischer@gmail.com>
2018-10-31 13:40:19 -0600

    🕓

--- public/js/createRoute.js
+++ public/js/createRoute.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   formSubmit()
 })
 
-//function for submitting create new post form
 function formSubmit() {
   let createBtn = document.getElementById('createSubmit')
   if (!createBtn) {
@@ -47,73 +46,12 @@ function formSubmit() {
       alert('Please login with your promoter code to enable promotion ________')
     }
 
-    console.log('post object:', newPostObj)
-    // axios.post that data to the correct backend route
     axios.post('/posts', newPostObj)
       .then((res) => {
-        console.log('create post res:', res)
-        // if (res) {
-        //   post_id = res.data
-        //   console.log('in the set post id:', post_id)
-        //   alert(`Created New Event!`)
-        // }
+        console.log('res:', res.data)
       })
-      // .then(() => {
-      //   console.log('started tag route')
-      //   //tags to be dealt with here
-      //   //deal with tag tagsArray
-      //   let newTags = e.target.elements[12].value
-      //   let newTagsArr = newTags.split(`, `)
-      //   let convertedTagsArr = []
-      // 
-      //   for (var i = 0; i < newTagsArr.length; i++) {
-      //     if (newTagsArr[i] === "OUTDOORS") {
-      //       convertedTagsArr.push(1)
-      //     }
-      //     if (newTagsArr[i] === "INDOORS") {
-      //       convertedTagsArr.push(2)
-      //     }
-      //     if (newTagsArr[i] === "21+") {
-      //       convertedTagsArr.push(3)
-      //     }
-      //     if (newTagsArr[i] === "BEER") {
-      //       convertedTagsArr.push(4)
-      //     }
-      //     if (newTagsArr[i] === "PIZZA") {
-      //       convertedTagsArr.push(5)
-      //     }
-      //     if (newTagsArr[i] === "RECURRING") {
-      //       convertedTagsArr.push(6)
-      //     }
-      //     if (newTagsArr[i] === "ICE CREAM") {
-      //       convertedTagsArr.push(7)
-      //     }
-      //     if (newTagsArr[i] === "FLASH EVENT") {
-      //       convertedTagsArr.push(8)
-      //     }
-      //     if (newTagsArr[i] === "COLLEGE/UNIVERSITY") {
-      //       convertedTagsArr.push(9)
-      //     }
-      //   }
-      //   console.log('convertedTagsArr', convertedTagsArr)
-      // 
-      //   //build up each post body using the converted tag array
-      //   convertedTagsArr.forEach(tag => {
-      //     let tagPostBody = {
-      //       post_id: post_id,
-      //       tag_id: tag
-      //     }
-      //     axios.post('/posts_tags', tagPostBody)
-      //       .then((res) => {
-      //         console.log(`Posted tags for post id ${post_id}`)
-      //         return res
-      //       })
-      //   })
-      //   return tags
-      // })
-      .catch((error) => {
-        console.log(error)
+      .catch((err) => {
+        console.log(err)
       })
-
   })
 }
